b9d3e4d3d08131c3661d1938afa51827d1c3f846
marselle.naz <101027445+marse11e@users.noreply.github.com>
2022-06-10 13:53:09 +0600

    Add files via upload

---
+++ class magazin.py
@@ -0,0 +1,160 @@
+from random import choice
+import os
+from time import sleep
+def clear():
+    if os.sys.platform == 'win32':
+        os.system('cls')
+    else:
+        os.system('clear')
+class Product:
+    def __init__(self, name, category, price) -> None:
+        self.name = name
+        self.category = category
+        self.price = price
+
+class Shelf:
+    def __init__(self, number, capacity, products:list) -> None:
+        self.number = number
+        self.capacity = capacity
+        self.products = products
+    def check_capacity(self):
+        if self.capacity < len(self.products):
+            return 'Перегруз'
+        else:
+            return 'Всё ок'
+    def get_products(self):
+        prod1 = []
+        for i in self.products:
+            prod1.append(i.name)
+        return prod1
+
+
+class Magazin:
+    def __init__(self, name1, shelfs:list) -> None:
+        self.name1 = name1
+        self.shelfs = shelfs
+
+    def get_products_all(self):
+        prod = []
+        for i in self.shelfs:
+            prod.extend(i.get_products())
+        return prod
+
+    def get_products_price(self):
+        d = {}
+        for i in self.shelfs:
+            for j in i.products:
+                d.update({
+                    j.name: j.price
+                })
+
+        return d
+    def get_all_price(self):
+        c = []
+        for i in self.shelfs:
+            for j in i.products:
+                c.append(j.price)
+        return f'Общая стоимость: {sum(c)}'
+    def get_categories(self):
+        list1 = []
+        
+        for i in self.shelfs:
+            for j in i.products:
+                list1.append(j.category)
+        set1 = set(list1)
+        return set1
+
+
+class User:
+    def __init__(self, name, money) -> None:
+        self.name = name
+        self.money = money
+        self.products = []
+     
+    def buy_product(self, shop):
+        print(f'У Покупателя {self.name} - {self.money} сом')
+        a = shop.get_products_price()
+        b =  choice(shop.get_products_all())
+        
+        for i in shop.shelfs:
+            for j in i.products:
+                if j.name == b:
+                    self.products.append(j)
+        self.money -= a[b]
+
+        return f'{self.name} купил , {b} за {a[b]} сом в {shop.name1}, у покупателя осталось {self.money}'
+
+    def get_info(self):
+        return f"""
+        Пользователь с именем {self.name}
+        Счет  {self.money}
+        Продукты {[i.name for i in self.products]}
+        """
+
+
+p = Product('Кола', 'Напиток', 45)
+p1 = Product('Сникерс', 'Шоколад', 50)
+p2 = Product('Сендвич', 'Еда', 70)
+p3 = Product('Винстон', 'Сигареты', 100)
+p4 = Product('Чай', 'Напитки', 190)
+p5 = Product('Самса', 'Еда', 55)
+p6 = Product('Вино', 'Алкоголь', 400)
+p7 = Product('Пельмени', 'Еда', 300)
+p8 = Product('Вареники', 'Еда', 180)
+p9 = Product('Хлеб', 'Выпечка', 20)
+
+
+s = Shelf(1, 4, [p, p1, p2, p3])
+s1 = Shelf(2, 3, [p4, p5, p6])
+sh2 = Shelf(3,3, [p7, p8, p9])
+
+
+
+m = Magazin('Гастроном', [s, s1, sh2])
+# u = User('Django', 500)
+# print(u.buy_product(m))
+
+
+
+
+def create_user(name, money):
+    return User(name, money)
+
+
+
+user = None
+while True:
+    
+    if user is None:    
+        print('Вы в новой игре зарегистритруйтесь')
+    
+    a = input("""
+        1) Что ты зарегистрироватся введите
+        2) Посмотреть все товары 
+        3) Информация о пользователе
+        4) Купить случайный продукт
+        5) Очистить
+        >>> """  )
+    
+    if a == '1':
+        name = input('Введите Имя: ')
+        money = int(input('Введите Количество денег: '))
+        user = create_user(name, money)
+    elif a == '2':
+        print(", ".join(m.get_products_all()))
+
+    elif a == '3':
+        if user is not None:
+            print(user.get_info())
+        else:
+            print('Зарегистрируйтесь!!')
+
+    elif a == '4':
+        if user is not None:
+            print(user.buy_product(m))
+        else:
+            print('Зарегистрируйтесь!!')
+    elif a == '5':
+        print('Идет очистка ...')
+        sleep(3)
+        clear()
