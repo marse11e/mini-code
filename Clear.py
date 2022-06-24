a7419036191c88f2039d3889e9fd469f512079a5
marselle.naz <101027445+marse11e@users.noreply.github.com>
2022-06-25 00:23:23 +0300

    Add files via upload

---
+++ Clear.py
@@ -0,0 +1,34 @@
+import time
+import os
+
+def clear():
+    # os.system('clear')
+    
+    
+    
+    
+    
+    # while True:
+    #     veryficirovaynie = input("""
+    #             [Yes - y] = [No - n]
+    #     >>> """)
+    #     if veryficirovaynie == 'y' or veryficirovaynie == 'Y' or veryficirovaynie == 'Yes' or veryficirovaynie == 'yes':
+    #         print('Очистка...')
+    #         time.sleep(3)
+    #         if os.sys.platform == 'win32':
+    #             os.system('cls')
+    #         else:
+    #             os.system('clear')
+    #     if veryficirovaynie == 'n' or veryficirovaynie == 'N' or veryficirovaynie == 'no' or veryficirovaynie == 'No':
+    #         break
+    #     else:
+    #         break
+    
+    
+    
+    
+    if os.sys.platform == 'win32':
+                os.system('cls')
+    else:
+        os.system('clear')
+clear()
