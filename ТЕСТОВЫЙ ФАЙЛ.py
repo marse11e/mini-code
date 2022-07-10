a93006ce8a5504648c3eaa3c36a4d60dc2f3143c
marselle.naz <101027445+marse11e@users.noreply.github.com>
2022-07-10 20:32:30 +0600

    Delete ТЕСТОВЫЙ ФАЙЛ.py

--- ТЕСТОВЫЙ ФАЙЛ.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-print('1 %')
-while True:
-    for i in range(2,101):
-        sleep(10)
-        print(i,'%')
-        if i == 100:
-            print('У вас сто процентов!')
-        
